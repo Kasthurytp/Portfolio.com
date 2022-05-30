5e6adfa4adffc4b3fba545470f70d6bfacd38937
Kasthury <t.kasthury66@gmail.com>
2022-05-30 14:22:02 +0530

    Updates

---
+++ precache-manifest.6848f949dba15ac92717c03bf13bbaa7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "829ea741ae44abb75da9",
+    "url": "/Portfolio.com/static/css/main.0c04adb2.chunk.css"
+  },
+  {
+    "revision": "829ea741ae44abb75da9",
+    "url": "/Portfolio.com/static/js/main.829ea741.chunk.js"
+  },
+  {
+    "revision": "45789c75afb661050078",
+    "url": "/Portfolio.com/static/js/1.45789c75.chunk.js"
+  },
+  {
+    "revision": "dea46daef4e1f25c4c47",
+    "url": "/Portfolio.com/static/css/2.88c14dd3.chunk.css"
+  },
+  {
+    "revision": "dea46daef4e1f25c4c47",
+    "url": "/Portfolio.com/static/js/2.dea46dae.chunk.js"
+  },
+  {
+    "revision": "a1d9b6eada5f14509f5d",
+    "url": "/Portfolio.com/static/js/runtime~main.a1d9b6ea.js"
+  },
+  {
+    "revision": "67dba4225f2fa9eecae361c4dde5993e",
+    "url": "/Portfolio.com/static/media/pexels-cottonbro-4065885.67dba422.jpg"
+  },
+  {
+    "revision": "625b543c35c0856dd740fa39e543eb71",
+    "url": "/Portfolio.com/static/media/pexels-j-kelly-brito-4067119.625b543c.jpg"
+  },
+  {
+    "revision": "5cab54e60190a5c562ec5f3b79e1f4bf",
+    "url": "/Portfolio.com/static/media/pexels-jess-bailey-designs-788946.5cab54e6.jpg"
+  },
+  {
+    "revision": "dc279bbd66e4da797c7bb4a386967e97",
+    "url": "/Portfolio.com/static/media/pexels-cottonbro-3945697.dc279bbd.jpg"
+  },
+  {
+    "revision": "4bd8a28e702295aeebbf46b09d403656",
+    "url": "/Portfolio.com/static/media/pexels-liza-summer-6347733.4bd8a28e.jpg"
+  },
+  {
+    "revision": "e407c1d2f1aa5b7ad245c43bb465ae69",
+    "url": "/Portfolio.com/static/media/pexels-marek-levak-2265484.e407c1d2.jpg"
+  },
+  {
+    "revision": "71e68c7c9e63acbbf90ce257886ee893",
+    "url": "/Portfolio.com/static/media/pexels-sound-on-3756879.71e68c7c.jpg"
+  },
+  {
+    "revision": "986c162c582ac62f6dc3e6ee8a5b79b5",
+    "url": "/Portfolio.com/index.html"
+  }
+];
